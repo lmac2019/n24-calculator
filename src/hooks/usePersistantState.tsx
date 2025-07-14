a7ab190bdb660bd6fcf35cb717263b9df80e7b7e
lmac2019 <liutr2014@gmail.com>
2025-07-13 17:27:16 -0700

    refine

--- src/hooks/usePersistantState.tsx
+++ src/hooks/usePersistantState.tsx
@@ -1,35 +1,37 @@
 import { useEffect, useState } from "react";
 
 type LocalStorageStateItem<T> = {
-    key: string;
-    defaultValue: T;
-    parser?: (value: string) => T;
-    serializer?: (value: T) => string;
-  };
-  
-  export function usePersistentState<T>(
-    { key, defaultValue, parser = JSON.parse, serializer = JSON.stringify }: LocalStorageStateItem<T>
-  ): [T, React.Dispatch<React.SetStateAction<T>>] {
-    const [state, setState] = useState<T>(() => {
-      const stored = localStorage.getItem(key);
-      if (stored !== null) {
-        try {
-          return parser(stored);
-        } catch (e) {
-          console.warn(`Failed to parse localStorage[${key}]:`, stored);
-        }
-      }
-      return defaultValue;
-    });
-  
-    useEffect(() => {
+  key: string;
+  defaultValue: T;
+  parser?: (value: string) => T;
+  serializer?: (value: T) => string;
+};
+
+export function usePersistentState<T>({
+  key,
+  defaultValue,
+  parser = JSON.parse,
+  serializer = JSON.stringify,
+}: LocalStorageStateItem<T>): [T, React.Dispatch<React.SetStateAction<T>>] {
+  const [state, setState] = useState<T>(() => {
+    const stored = localStorage.getItem(key);
+    if (stored !== null) {
       try {
-        localStorage.setItem(key, serializer(state));
+        return parser(stored);
       } catch (e) {
-        console.warn(`Failed to serialize localStorage[${key}]`, state);
+        console.warn(`Failed to parse localStorage[${key}]:`, stored);
       }
-    }, [key, state]);
-  
-    return [state, setState];
-  }
-  
+    }
+    return defaultValue;
+  });
+
+  useEffect(() => {
+    try {
+      localStorage.setItem(key, serializer(state));
+    } catch (e) {
+      console.warn(`Failed to serialize localStorage[${key}]`, state);
+    }
+  }, [key, serializer, state]);
+
+  return [state, setState];
+}
